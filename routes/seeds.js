ae4bf24657e52cc3e072a09e9d2722bcc7396c6f
Art <arttay1090@gmail.com>
2016-03-13 09:18:03 -0500

    refact seeds

--- routes/seeds.js
+++ routes/seeds.js
@@ -1,4 +1,5 @@
 var api = require("../services/Services");
+var service = require("../services/seedsService.js")
 var json = require("../public/data/seeds.json");
 var Q = require("q");
 var _ = require("underscore");
@@ -21,55 +22,10 @@ module.exports = function (app) {
 			    	var category 	= query.category.split(".")[0];
 			    	var subCategory = query.category.split(".")[1];
 
+			   		service.init(category, subCategory).then((data) => {
+						reply(data);
+			   		});
 			   		
-			    	category = api.commonCore.searchJsonObject(json, category);
-			    	
-
-			    	if (subCategory === undefined) {
-			    		//user passed a top level category(seeds, ammo, bolts ect)
-			    		for (var key in category) {
-			    			var item = category[key]
-			    			if(!_.isEmpty(item)) {
-			    				for (var ke in item) {
-			    					//todo: make this loop more dynamic
-			    					//todo: also, I really really hate this, make it better
-			    					arr.push(api.itemPrice.getPrice(item[ke])); //grabs the price of an item from the current api and pushes it into a array as a promise 
-				    			}
-			    			}
-			    		}
-			    	}
-
-			    	 else {
-			    		data = api.commonCore.searchJsonObject(category, subCategory);
-			    		for (var key in data) {
-			    			arr.push(api.itemPrice.getPrice(data[key]));
-			    		}
-			    	}
-			    	
-			    	
-			    	Q.all(arr).done(function (data) {
-			    		for (var key in data) {
-			    			var item = JSON.parse(data[key]);
-			    			var name = item.item.name
-			    			var currentPrice = item.item.current.price;
-			    			if(typeof currentPrice === "string") {
-			    				//price is in the format of "100.6k"
-			    				currentPrice = parseInt(api.commonCore.convertToGold(currentPrice));
-			    			}
-
-			    			var obj = {
-			    				displayName: name,
-			    				id: item.item.id,
-			    				description: item.item.description,
-			    				price: currentPrice,
-			    				graph: "http://services.runescape.com/m=itemdb_rs/api/graph/" + item.item.id + ".json"
-			    			};
-			    			itemArr.push(obj);	
-			    		}
-			    		var returnData = api.commonCore.optionsRouter(itemArr, query);
-			    		returnData === null ? reply(itemArr) : reply(returnData);
-			    	});
-	
 			   	} else {
 					reply(itemArr);   
 			    }
